5b0f48d5939b57fe74042ecbbf69435033d19fb1
Shravan Revanna <94772842+githubhosting@users.noreply.github.com>
2023-02-14 21:17:00 +0530

    Create BasicLinkedList.c

---
+++ BasicLinkedList.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+void add_node(int value) {
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->data = value;
+    new_node->next = head;
+    head = new_node;
+}
+
+void print_list() {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, val;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &val);
+        add_node(val);
+    }
+    print_list();
+    return 0;
+}
